794164734db2d4b04cd207fb4715958230cbbcbd
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2016-09-16 02:05:37 +0300

    lisätty aikatarkastelua ja tulostusta

--- 15-puzzle/src/Main.java
+++ 15-puzzle/src/Main.java
@@ -23,6 +23,7 @@ public class Main {
         int value;
         int[][] a;
         int i = 1;
+        long start = System.currentTimeMillis();
         while (true) {
             Game g = q.poll();
 //            System.out.println("g:");
@@ -32,7 +33,14 @@ public class Main {
             if (g.getY() - 1 >= 0 && g.d != Direction.DOWN) {
                 a = Logic.moveBlock(g.getTable(), g.getY(), g.getX(), Direction.UP);
                 value = Logic.countManhattan(a);
-                q.add(new Game(a, g.getX(), g.getY() - 1, value + g.moves +1, Direction.UP, g.moves +1));
+                if (value == 0) {
+                    print(a);
+                    System.out.println("ratkaisu");
+                    System.out.println(i);
+                    System.out.println(g.moves + 1);
+                    break;
+                }
+                q.add(new Game(a, g.getX(), g.getY() - 1, value + g.moves + 1, Direction.UP, g.moves + 1));
                 minV = Math.min(value, minV);
 //                print(a);
 //                System.out.println(value + "\n");
@@ -40,7 +48,14 @@ public class Main {
             if (g.getY() + 1 < 3 && g.d != Direction.UP) {
                 a = Logic.moveBlock(g.getTable(), g.getY(), g.getX(), Direction.DOWN);
                 value = Logic.countManhattan(a);
-                q.add(new Game(a, g.getX(), g.getY() + 1, value + g.moves +1, Direction.DOWN, g.moves +1));
+                if (value == 0) {
+                    print(a);
+                    System.out.println("ratkaisu");
+                    System.out.println(i);
+                    System.out.println(g.moves + 1);
+                    break;
+                }
+                q.add(new Game(a, g.getX(), g.getY() + 1, value + g.moves + 1, Direction.DOWN, g.moves + 1));
                 minV = Math.min(value, minV);
 //                print(a);
 //                System.out.println(value + "\n");
@@ -48,7 +63,14 @@ public class Main {
             if (g.getX() - 1 >= 0 && g.d != Direction.RIGHT) {
                 a = Logic.moveBlock(g.getTable(), g.getY(), g.getX(), Direction.LEFT);
                 value = Logic.countManhattan(a);
-                q.add(new Game(a, g.getX() - 1, g.getY(), value + g.moves +1, Direction.LEFT, g.moves +1));
+                if (value == 0) {
+                    print(a);
+                    System.out.println("ratkaisu");
+                    System.out.println(i);
+                    System.out.println(g.moves + 1);
+                    break;
+                }
+                q.add(new Game(a, g.getX() - 1, g.getY(), value + g.moves + 1, Direction.LEFT, g.moves + 1));
                 minV = Math.min(value, minV);
 //                print(a);
 //                System.out.println(value + "\n");
@@ -56,20 +78,23 @@ public class Main {
             if (g.getX() + 1 < 3 && g.d != Direction.LEFT) {
                 a = Logic.moveBlock(g.getTable(), g.getY(), g.getX(), Direction.RIGHT);
                 value = Logic.countManhattan(a);
-                q.add(new Game(a, g.getX() + 1, g.getY(), value + g.moves +1, Direction.RIGHT, g.moves +1));
+                if (value == 0) {
+                    print(a);
+                    System.out.println("ratkaisu");
+                    System.out.println(i);
+                    System.out.println(g.moves + 1);
+                    break;
+                }
+                q.add(new Game(a, g.getX() + 1, g.getY(), value + g.moves + 1, Direction.RIGHT, g.moves + 1));
                 minV = Math.min(value, minV);
 //                print(a);
 //                System.out.println(value + "\n");
             }
-            if (minV == 0) {
-                System.out.println("ratkaisu");
-                System.out.println(i);
-                System.out.println(g.moves +1);
-                break;
-            }
+
             i++;
 
         }
+        System.out.println(System.currentTimeMillis()-start);
     }
 
     static void print(int[][] t) {
